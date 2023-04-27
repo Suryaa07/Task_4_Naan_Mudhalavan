d1afb6abd5626b66c943160ab4c6afaff8964765
K. Suryaa Narayanan <72430187+Suryaa07@users.noreply.github.com>
2023-04-27 20:27:48 +0530

    Update db.js

--- db.js
+++ db.js
@@ -28,3 +28,48 @@ app.post('/users', async (req, res) => {
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
+app.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({ users });
+  } catch (err) {
+    res.status(500).json({ message: 'Error getting users', error: err });
+  }
+});
+app.get('/users/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const user = await User.findById(id);
+    res.status(200).json({ user });
+  } catch (err) {
+    res.status(500).json({ message: 'Error getting user', error: err });
+  }
+});
+app.put('/users/:id', async (req, res) => {
+  const { id } = req.params;
+  const { name, email, password } = req.body;
+
+  try {
+    const user = await User.findById(id);
+    user.name = name;
+    user.email = email;
+    user.password = password;
+    await user.save();
+    res.status(200).json({ message: 'User updated successfully' });
+  } catch (err) {
+    res.status(500).json({ message: 'Error updating user', error: err });
+  }
+});
+app.delete('/users/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    await User.findByIdAndDelete(id);
+    res.status(200).json({ message: 'User deleted successfully' });
+  } catch (err) {
+    res.status(500).json({ message: 'Error deleting user', error: err });
+  }
+});
+
+
